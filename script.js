440a337472df3aa986d47952eadacc529998b58c
jonathasobelo <jonathasobelo@gmail.com>
2022-06-21 23:23:01 -0300

    desafio iniciado

---
+++ script.js
@@ -0,0 +1,15 @@
+var a= 4;
+var b= 4;
+
+function numeros(a , b){
+ return a + b;
+
+}
+//numeros(2,6);
+console.log(numeros(a,b));
+if(a == b) { 
+  console.log("iguais")
+}
+else if(a + b > 10){
+        console.log("é maior que 10")
+}
